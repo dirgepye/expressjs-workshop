d66ce096139c5da4af72bfb69b6aa26a8b18ed68
dirgepye <s.stenhouse87@gmail.com>
2015-11-30 22:41:51 +0000

    Exercise 4: Fetching data!

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/entry/:entryId', function(req, res) {
+
+  var entries = {
+    1: {
+      firstName: "John",
+      lastName: "One",
+      emails: [{
+        type: "home",
+        address: "john@smith.com"
+      }, {
+        type: "work",
+        address: "jsmith@megacorp.com"
+      }]
+    },
+    2: {
+      firstName: "John",
+      lastName: "Two",
+      emails: [{
+        type: "home",
+        address: "john@smith.com"
+      }, {
+        type: "work",
+        address: "jsmith@megacorp.com"
+      }]
+    },
+    3: {
+      firstName: "John",
+      lastName: "Three",
+      emails: [{
+        type: "home",
+        address: "john@smith.com"
+      }, {
+        type: "work",
+        address: "jsmith@megacorp.com"
+      }]
+    },
+
+  };
+  
+  if (req.params.entryId <= Object.keys(entries).length) {
+    res.json(entries[req.params.entryId]);
+  }
+  else {
+    res.sendStatus(404);
+  }
 });
+
 
 
 
@@ -12,7 +56,7 @@ app.get('/', function (req, res) {
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function() {
   var host = server.address().address;
   var port = server.address().port;
 
