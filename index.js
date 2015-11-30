ca3e0d9907acef18f8511cfc2f24645569dde685
dirgepye <s.stenhouse87@gmail.com>
2015-11-30 19:31:38 +0000

    Exercise 2: A wild paramater appears!

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/hello/:firstName', function (req, res) {
+  var name = req.params.firstName;
+  res.send("<h1>Hello " + name + "</h1>");
 });
 
 
